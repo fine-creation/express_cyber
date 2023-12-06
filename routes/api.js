338dc567a1aa96d4edeb59f98204068a8867f797
RYUJI TATEMOTO <tatemoto@fine-creation.jp>
2023-12-06 11:50:09 +0900

    api

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
-const cors = require('cors'); // corsミドルウェアを追加
+const cors = require('cors');
 
-// corsミドルウェアを使用
 router.use(cors());
 
 router.get('/', async (req, res) => {
